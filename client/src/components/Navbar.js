2d0197b05a6a75d80e1df837963dcd7af8721165
Ravi-nanda-bala <ruderavi1998@gmail.com>
2021-12-15 12:00:51 +0530

    first commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+function Navbar() {
+
+  function logout() {
+    localStorage.removeItem('currentUser')
+    window.location.href='/login'
+  }
+
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="/">
+        Book-A-Trip
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"><i className='fa fa-bars' style={{color: 'white'}}></i></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNav">
+
+         
+
+
+          <ul class="navbar-nav ml-auto">
+
+          {localStorage.getItem('currentUser') ? (
+            <div class="dropdown mr-5">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-user" aria-hidden="true"></i>  {JSON.parse(localStorage.getItem('currentUser')).name} 
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="/profile">Profile</a>
+              <a class="dropdown-item" href="#" onClick={logout}>Logout</a>
+            </div>
+          </div>
+
+          ) : (
+            <>
+            <li class="nav-item active">
+              <a class="nav-link" href="/register">
+                Register
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/login">
+                Login
+              </a>
+            </li>
+            </>
+            )}
+           
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = 'mongodb+srv://ravi:ravi1998@cluster0.eylp9.mongodb.net/room?retryWrites=true&w=majority'
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
